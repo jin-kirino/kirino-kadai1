08e3682a238989135090332bfd364a3e207aa25c
jin-kirino <jinkirino@gmail.com>
2023-02-08 09:26:51 +0900

    課題１提出

--- kirino-kadai1/kirino-kadai1/ContentView.swift
+++ kirino-kadai1/kirino-kadai1/ContentView.swift
@@ -8,12 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstText: String = ""
+    @State private var secondText: String = ""
+    @State private var thirdText: String = ""
+    @State private var fourthText: String = ""
+    @State private var fifthText: String = ""
+    @State private var sumOfAdditions: String = "Label"
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            VStack {
+                TextField("", text: $firstText)
+                    .frame(width: 100)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("", text: $secondText)
+                    .frame(width: 100)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("", text: $thirdText)
+                    .frame(width: 100)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("", text: $fourthText)
+                    .frame(width: 100)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("", text: $fifthText)
+                    .frame(width: 100)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button {
+                    let firstNumber = Int(firstText) ?? 0
+                    let secondNumber = Int(secondText) ?? 0
+                    let thirdNumber = Int(thirdText) ?? 0
+                    let fourthNumber = Int(fourthText) ?? 0
+                    let fifthNumber = Int(fifthText) ?? 0
+                    let calculationResults = firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber
+                    print(firstNumber + secondNumber + thirdNumber + fourthNumber + fifthNumber)
+                    sumOfAdditions = String(calculationResults)
+                } label: {
+                    Text("Button")
+                }
+                Text(sumOfAdditions)
+                    .padding(EdgeInsets(
+                        top: 3,
+                        leading: 0,
+                        bottom: 0,
+                        trailing: 40
+                    ))
+                    .frame(width: 100.0)
+                    .multilineTextAlignment(.leading)
+                Spacer()
+            }
+            Spacer()
         }
         .padding()
     }
